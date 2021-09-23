4ed161ebc924e8c77fb9a35f7aa53664e4dcd352
Rainethhh <Rainethcubed@gmail.com>
2021-09-22 21:41:07 -0600

    found sources
    Found a couple sources for generating regular n-simplices

---
+++ convexHullOperations.py
@@ -0,0 +1,7 @@
+
+
+
+def selectDirections(dims):
+	# https://math.stackexchange.com/questions/2533532/how-to-find-the-coordinates-of-an-n-dimensional-rectangular-simplex
+	# https://en.wikipedia.org/wiki/Simplex
+	pass
