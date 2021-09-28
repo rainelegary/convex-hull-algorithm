b82c8138417d7b538775d9ccaddfe1603dff323f
Rainethhh <Rainethcubed@gmail.com>
2021-09-27 20:57:56 -0600

    direction selecting
    The program can now choose the starting directions. It can also generate a new direction based on one of a simplex's faces

--- convexHullOperations.py
+++ convexHullOperations.py
@@ -1,10 +1,19 @@
-from matrixOperations import matMul, transpose, matInverse
+from matrixOperations import flipVector, identityMatrix, matMul, sumVectors, transpose, matInverse
+from linearAlgebra import pointsToPlane
+import math
 
 
 def selectFirstDirections(dims): 
 	# https://math.stackexchange.com/questions/2533532/how-to-find-the-coordinates-of-an-n-dimensional-rectangular-simplex
 	# https://en.wikipedia.org/wiki/Simplex
-	pass
+	a = (1 + math.sqrt(dims + 1))/dims
+	translationConst = -(a+1)/(dims+1)
+
+	simplex = [[0 + translationConst] * dims] * dims
+	for dim in range(dims): simplex[dim][dim] = 1 + translationConst
+	
+	simplex.append([a + translationConst] * dims)
+	return simplex
 
 
 def newDirection(oldSimplex, missingVertex):
@@ -12,10 +21,14 @@ def newDirection(oldSimplex, missingVertex):
 	# - find scalar form of plane
 	# - each directional component of the vector will be a constant from the plane's scalar form
 	# invert that direction; that's the direction this function should return
+	oldVertex = oldSimplex[missingVertex]
 	oldSimplex.pop(missingVertex)
+	testPoint = oldSimplex[0]
+	dVec = pointsToPlane(oldSimplex)[0]
+	if pointDistance(sumVectors([dVec, testPoint]), oldVertex) < pointDistance(testPoint, oldVertex): dVec = flipVector(dVec)
+	return dVec
+
 	
-
-
 
 def defineHullCenter(points): # prob won't actually need this function
 	nPoints = len(points)
@@ -27,3 +40,8 @@ def defineHullCenter(points): # prob won't actually need this function
 	return centerPoint
 
 
+def pointDistance(pointA, pointB):
+	return math.sqrt(sum(pointA[i]*pointB[i] for i in range(len(pointA))))
+
+
+print(newDirection(identityMatrix(3) + [[1, 2, 3]], 0))
--- linearAlgebra.py
+++ linearAlgebra.py
@@ -1,18 +1,24 @@
-from matrixOperations import dotProduct, matMul, subtractVectors, matInverse, transpose
+from matrixOperations import dotProduct, identityMatrix, matMul, subtractVectors, matInverse, transpose
 from random import random
 
 def pointsToPlane(points):
 
 	vectorMat = [subtractVectors(points[0], points[i]) for i in range(1, len(points))]
 	vectorMat.append([random() for i in range(len(points[0]))])
-	dotProdVec = [[0] for i in range(len(points) - 1)]
-	dotProdVec.append([1])
+
+	dotProdVec = transpose([[0] * (len(points) - 1) + [1]])
+
 	constantsVec = matMul(matInverse(vectorMat), dotProdVec)
 	constants = transpose(constantsVec)[0]
 	quantity = dotProduct(constants, points[0])
 	
 	plane = (constants, quantity)
 	return plane
+
+
+# print(pointsToPlane([[4, 0, 3], [0, 0, 1], [10, 0, 5]]))
+# print(pointsToPlane(identityMatrix(3)))
+
 
 	
 
